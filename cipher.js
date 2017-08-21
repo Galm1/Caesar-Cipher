5f18f885c2113b56071b50298697e9bbaee27a32
EliGarcia <garciaelisha93@yahoo.com>
2017-08-21 15:14:55 -0500

    initial

---
+++ cipher.js
@@ -0,0 +1,38 @@
+// made an array of the alphabet twice so i can use it for the cipher. twice so i can extend beyond the first.
+let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+function encipher(str, shift) {
+  // let str = 'hello';
+  // let shift = 20;
+  console.log('input is ' + str);
+  let newWord = [];
+
+  let arr = str.split('');
+  for (var i = 0; i < arr.length; i++) {
+    let letter = arr[i];
+    for (var j = 0; j < alpha.length; j++) {
+      if (letter === alpha[j]) {
+        let newLetter = alpha[j + shift];
+        newWord.push(newLetter);
+        console.log(newWord);
+        break;
+      };
+    }
+  }
+  let cipherWord = newWord.join('');
+  console.log('the output is ' + cipherWord);
+  return cipherWord;
+};
+encipher();
+
+
+
+
+
+// function decipher(str, shift) {}
+
+// module.exports = {
+//   encipher,
+//   decipher
+// }
